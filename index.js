83bce20011fa702583bbbd72b2a338a0b941ecaa
shubhamrawat0224 <66406374+shubhamrawat0224@users.noreply.github.com>
2025-07-15 11:24:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const eventRoutes = require('./routes/eventRoutes');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', authRoutes);
